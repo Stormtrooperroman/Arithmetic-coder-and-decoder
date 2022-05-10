f456587de3b6ee68072c9e39f16922bc58da1156
Stormtrooperroman <stormtrooperroman@gmail.com>
2022-05-10 19:23:15 +0700

    Google c++ style add

--- coder.cpp
+++ coder.cpp
@@ -1,233 +1,221 @@
+#include <sys/stat.h>
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
-#include <sys/stat.h>
 #include <stdexcept>
 
-using namespace std;
 
-int indexForSymbol (char c, vector<pair<char, unsigned int>> arr){
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (c == arr[i].first){
+int indexForSymbol(char c, std::vector<std::pair<char, unsigned int>> arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (c == arr[i].first) {
             return i+2;
         }
     }
 
     return -1;
-    
 }
 
-void output_bit (unsigned int bit, unsigned int* bit_len, unsigned char* write_bit, FILE* output_file){
+void output_bit(unsigned int bit, unsigned int* bit_len,
+                unsigned char* write_bit, FILE* output_file) {
     (*write_bit) >>= 1;
     if (bit) (*write_bit) |= 0x80;
     (*bit_len)--;
-    if ( (*bit_len) == 0 ){
-        fputc( (*write_bit), output_file );
+    if ( (*bit_len) == 0 ) {
+        fputc((*write_bit), output_file);
         (*bit_len) = 8;
     }
 }
 
-void bitsPlusFollow (unsigned int bit, unsigned int* bits_to_follow, unsigned int* bit_len, unsigned char* write_bit, FILE* output_file){
+void bitsPlusFollow(unsigned int bit, unsigned int* bits_to_follow,
+                    unsigned int* bit_len, unsigned char* write_bit,
+                    FILE* output_file) {
     output_bit(bit, bit_len, write_bit, output_file);
 
-    for (; *bits_to_follow > 0; (*bits_to_follow)--){
+    for (; *bits_to_follow > 0; (*bits_to_follow)--) {
         output_bit(!bit, bit_len, write_bit, output_file);
     }
 }
-double coder (const char* input_name="input.txt", const char* output_name="encoded.txt"){
-    unsigned int * alfabet = new unsigned int [256];
-    for (int i=0; i<256; i++){
+
+double coder(const char* input_name = "input.txt",
+                const char* output_name = "encoded.txt") {
+    unsigned int * alfabet = new unsigned int[256];
+    for (int i = 0; i < 256; i++) {
         alfabet[i] = 0;
     }
 
-    FILE* input_file = fopen(input_name, "rb"); 
+    FILE* input_file = fopen(input_name, "rb");
     if (input_file == nullptr) {
-       throw invalid_argument("File not found.");
+       throw std::invalid_argument("File not found.");
     }
 
-    unsigned long long coll_characters = 0;
     unsigned char character = 0;
-    while (!feof(input_file)) { // Read from input file
+
+    while (!feof(input_file)) {  // Read from input file
        character = fgetc(input_file);
-       if (!feof(input_file)){
-           coll_characters++;
+       if (!feof(input_file)) {
            alfabet[character]++;
        }
     }
-    
+
     fclose(input_file);
 
-    vector<pair<char, unsigned int>> arr;
-    for (int i=0; i<256; i++){
-        if (alfabet[i] != 0){
-            arr.push_back(make_pair((char)i, alfabet[i]));
+    std::vector<std::pair<char, unsigned int>> arr;
+    for (int i=0; i < 256; i++) {
+        if (alfabet[i] != 0) {
+            arr.push_back(std::make_pair(static_cast<char>(i), alfabet[i]));
         }
     }
 
     sort(arr.begin(), arr.end(),
-            [](const pair<char, unsigned int> &l, const pair<char, unsigned int> &r)
-            {
+            [](const std::pair<char, unsigned int> &l,
+                const std::pair<char, unsigned int> &r) {
                 if (l.second != r.second) {
                     return l.second >= r.second;
                 }
- 
+
                 return l.first < r.first;
             });
 
 
-    unsigned long long* table = new unsigned long long[arr.size() + 2];
+    uint16_t* table = new uint16_t[arr.size() + 2];
     table[0] = 0;
     table[1] = 1;
-    for (int i = 0; i < arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++) {
         unsigned int b = arr[i].second;
-        for (int j = 0; j<i; j++){
+        for (int j = 0; j < i; j++) {
             b += arr[j].second;
         }
         table[i+2] = b;
     }
 
-    if (table[arr.size()] > (1<<14 -1)){
-        throw invalid_argument("Error, too long count.");
+    if (table[arr.size()] > (1<<14 -1)) {
+        throw std::invalid_argument("Error, too long count.");
     }
-    unsigned long long low_v = 0;
-    unsigned long long high_v = (((unsigned long long)(1)<<16) - 1);
-    unsigned long long delitel = table[arr.size()+1];
-    unsigned long long diff = high_v - low_v + 1;
-    unsigned long long first_qtr = (high_v + 1) / 4;
-    unsigned long long half = first_qtr * 2;
-    unsigned long long third_qtr = first_qtr * 3;
+
+    unsigned int low_v = 0;
+    unsigned int high_v = ((static_cast<unsigned int>(1) << 16) - 1);
+
+    unsigned int delitel = table[arr.size()+1];
+    unsigned int diff = high_v - low_v + 1;
+    unsigned int first_qtr = (high_v + 1) / 4;
+    unsigned int half = first_qtr * 2;
+    unsigned int third_qtr = first_qtr * 3;
+
     unsigned int bits_to_follow = 0;
     unsigned int bit_len = 8;
     unsigned char write_bit = 0;
+
     int j = 0;
-    
-	input_file = fopen(input_name, "rb"); 
+
+    input_file = fopen(input_name, "rb");
     FILE* output_file = fopen(output_name, "wb +");
 
     char col_letters = arr.size();
     fputc(col_letters, output_file);
 
-    for (int i=0; i<256; i++){ // Writing the letters used and their number
-        if (alfabet[i] != 0){
-            fputc((char)i, output_file);
-            fwrite(reinterpret_cast<const char*>(&alfabet[i]), sizeof(unsigned int), 1, output_file);
+    // Writing the letters used and their number
+    for (int i = 0; i < 256; i++) {
+        if (alfabet[i] != 0) {
+            fputc(static_cast<char>(i), output_file);
+            fwrite(reinterpret_cast<const char*>(&alfabet[i]),
+                    sizeof(unsigned int), 1, output_file);
         }
     }
 
-    int i = 0;
-	while (!feof(input_file)) { // Read from input file
 
+    while (!feof(input_file)) {  // Read from input file
         character = fgetc(input_file);
 
-        if (!feof(input_file)){
-
+        if (!feof(input_file)) {
             j = indexForSymbol(character, arr);
-            i++;
 
             high_v = low_v +  table[j] * diff / delitel - 1;
             low_v = low_v + table[j - 1] * diff  / delitel;
 
-            for (;;){
-                
-                
-                
-                if (high_v < half){
-                    bitsPlusFollow(0, &bits_to_follow, &bit_len, &write_bit, output_file);
-                }
-
-                else if (low_v >= half){
-                    bitsPlusFollow(1, &bits_to_follow, &bit_len, &write_bit, output_file);
+            for (;;) {
+                if (high_v < half) {
+                    bitsPlusFollow(0, &bits_to_follow,
+                                    &bit_len, &write_bit, output_file);
+                } else if (low_v >= half) {
+                    bitsPlusFollow(1, &bits_to_follow,
+                                    &bit_len, &write_bit, output_file);
                     low_v -= half;
                     high_v -= half;
-                }
-
-                else if ((low_v >= first_qtr) && (high_v < third_qtr)){
+                } else if ((low_v >= first_qtr) && (high_v < third_qtr)) {
                     bits_to_follow++;
                     low_v -= first_qtr;
                     high_v -= first_qtr;
+                } else {
+                    break;
                 }
-                else break;
 
                 low_v += low_v;
                 high_v += high_v + 1;
-
-
             }
-        }
-        else{
-        
+        } else {
             high_v = low_v +  table[1] * diff / delitel - 1;
             low_v = low_v + table[0] * diff  / delitel;
-        
 
-            for (;;){
-                
-                
-                if (high_v < half){
-                    bitsPlusFollow(0, &bits_to_follow, &bit_len, &write_bit, output_file);
-                }
-
-                else if (low_v >= half){
-                    bitsPlusFollow(1, &bits_to_follow, &bit_len, &write_bit, output_file);
+            for (;;) {
+                if (high_v < half) {
+                    bitsPlusFollow(0, &bits_to_follow,
+                                    &bit_len, &write_bit, output_file);
+                } else if (low_v >= half) {
+                    bitsPlusFollow(1, &bits_to_follow,
+                                    &bit_len, &write_bit, output_file);
                     low_v -= half;
                     high_v -= half;
-                }
-
-                else if ((low_v >= first_qtr) && (high_v < third_qtr)){
+                } else if ((low_v >= first_qtr) && (high_v < third_qtr)) {
                     bits_to_follow++;
                     low_v -= first_qtr;
                     high_v -= first_qtr;
+                } else {
+                    break;
                 }
-                else break;
 
                 low_v += low_v;
                 high_v += high_v + 1;
-
-
             }
+
             bits_to_follow+=1;
-            
-            if (low_v < first_qtr){
-                bitsPlusFollow(0, &bits_to_follow, &bit_len, &write_bit, output_file);
-            }
-            else {
-                bitsPlusFollow(1, &bits_to_follow, &bit_len, &write_bit, output_file);
+
+            if (low_v < first_qtr) {
+                bitsPlusFollow(0, &bits_to_follow,
+                                &bit_len, &write_bit, output_file);
+            } else {
+                bitsPlusFollow(1, &bits_to_follow,
+                                &bit_len, &write_bit, output_file);
             }
 
             write_bit >>= bit_len;
             fputc(write_bit, output_file);
-            
         }
         diff = high_v - low_v + 1;
-	}
-
+    }
 
     fclose(input_file);
     fclose(output_file);
 
-    unsigned long long file_full_size = 0;
-    unsigned long long commpres_size = 0;
+    unsigned int file_full_size = 0;
+    unsigned int commpres_size = 0;
     struct stat sb{};
     struct stat se{};
     // Finding compression ratio
     if (!stat(input_name, &sb)) {
         file_full_size = sb.st_size;
-    }
-    else {
+    } else {
         perror("stat");
     }
     if (!stat(output_name, &se)) {
         commpres_size = se.st_size;
-    }
-    else {
+    } else {
         perror("stat");
     }
     return (commpres_size + 0.0 ) / file_full_size;
 }
 
-int main(){
-    cout<<coder()<<endl; // Print compression ratio
+int main() {
+    std::cout << coder() << std::endl;  // Print compression ratio
 }
--- decoder.cpp
+++ decoder.cpp
@@ -5,170 +5,148 @@
 #include <algorithm>
 #include <stdexcept>
 
-using namespace std;
 
-int input_bit (unsigned char* read_bit, unsigned int* bit_len, FILE* input_file, unsigned int* garbage_bit){
-    if ( (*bit_len) == 0 ){
-        (*read_bit) = fgetc(input_file); 
-        if (feof(input_file)){
+int input_bit(unsigned char* read_bit, unsigned int* bit_len,
+                FILE* input_file, unsigned int* garbage_bit) {
+    if ( (*bit_len) == 0 ) {
+        (*read_bit) = fgetc(input_file);
+        if (feof(input_file)) {
             (*garbage_bit)++;
-            if ( (*garbage_bit)>14 ){
-                throw invalid_argument("Can't decompress");
+            if ( (*garbage_bit) > 14 ) {
+                throw std::invalid_argument("Can't decompress");
             }
-            // return -1;
         }
         (*bit_len) = 8;
     }
     int t = (*read_bit) & 1;
     (*read_bit) >>= 1;
     (*bit_len)--;
-    return t; 
+    return t;
 }
 
-int indexForSymbol (char c, vector<pair<char, unsigned int>> arr){
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (c == arr[i].first){
-            return i+1;
-        }
-    }
 
-    return -1;
-    
-}
-
-void decoder (const char* input_name="encoded.txt", const char* output_name="output.txt"){ // Decoding function
-    unsigned int * alfabet = new unsigned int [256];
-    for (int i=0; i<256; i++){
+void decoder(const char* input_name = "encoded.txt",
+                const char* output_name = "output.txt") {  // Decoding function
+    unsigned int * alfabet = new unsigned int[256];
+    for (int i = 0; i < 256; i++) {
         alfabet[i] = 0;
     }
-    FILE* input_file = fopen(input_name, "rb"); // Open input file
+    FILE* input_file = fopen(input_name, "rb");  // Open input file
     if (input_file == nullptr) {
-       throw invalid_argument("File not found.");
+       throw std::invalid_argument("File not found.");
     }
     unsigned char col = 0;
     unsigned int col_letters = 0;
     col = fgetc(input_file);
-    if (!feof(input_file)){
-       col_letters =(unsigned int) col;
+    if (!feof(input_file)) {
+       col_letters = static_cast<unsigned int>(col);
     }
 
     unsigned char character = 0;
-    for (int i = 0; i< col_letters; i++){ // Reading the letters used and their number
+    // Reading the letters used and their number
+    for (int i = 0; i < col_letters; i++) {
         character = fgetc(input_file);
-        if (!feof(input_file)){
-            fread(reinterpret_cast<char*>(&alfabet[character]), sizeof(unsigned int), 1, input_file);
-        }
-        else {
-            throw invalid_argument("Can't decompress file.");
+        if (!feof(input_file)) {
+            fread(reinterpret_cast<char*>(&alfabet[character]),
+                    sizeof(unsigned int), 1, input_file);
+        } else {
+            throw std::invalid_argument("Can't decompress file.");
         }
     }
-    unsigned long long coll_characters = 0;
-    vector<pair<char, unsigned int>> arr;
-    for (int i=0; i<256; i++){
-        if (alfabet[i] != 0){
-            coll_characters += alfabet[i];
-            arr.push_back(make_pair((char)i, alfabet[i]));
+
+    std::vector<std::pair<char, unsigned int>> arr;
+    for (int i = 0; i < 256; i++) {
+        if (alfabet[i] != 0) {
+            arr.push_back(std::make_pair(static_cast<char>(i), alfabet[i]));
         }
     }
 
     sort(arr.begin(), arr.end(),
-            [](const pair<char, unsigned int> &l, const pair<char, unsigned int> &r)
-            {
+            [](const std::pair<char, unsigned int> &l,
+                const std::pair<char, unsigned int> &r) {
                 if (l.second != r.second) {
                     return l.second >= r.second;
                 }
- 
                 return l.first < r.first;
             });
 
 
-    unsigned long long* table = new unsigned long long[arr.size() + 2];
+    uint16_t* table = new uint16_t[arr.size() + 2];
     table[0] = 0;
     table[1] = 1;
-    for (int i = 0; i < arr.size(); i++){
+    for (int i = 0; i < arr.size(); i++) {
         unsigned int b = arr[i].second;
-        for (int j = 0; j<i; j++){
+        for (int j = 0; j < i; j++) {
             b += arr[j].second;
         }
         table[i+2] = b;
     }
 
-    
-    unsigned long long low_v = 0;
-    unsigned long long high_v = (((unsigned long long)(1)<<16) - 1);
-    unsigned long long delitel = table[arr.size()+1];
-    unsigned long long first_qtr = (high_v + 1) / 4;
-    unsigned long long half = first_qtr * 2;
-    unsigned long long third_qtr = first_qtr * 3;
+    unsigned int low_v = 0;
+    unsigned int high_v = ((static_cast<unsigned int>(1) << 16) - 1);
+    unsigned int delitel = table[arr.size()+1];
+    unsigned int first_qtr = (high_v + 1) / 4;
+    unsigned int half = first_qtr * 2;
+    unsigned int third_qtr = first_qtr * 3;
     FILE* output_file = fopen(output_name, "wb +");
     unsigned int bit_len = 0;
     unsigned char read_bit = 0;
     unsigned int garbage_bit = 0;
-    unsigned short value = 0;
+    uint16_t value = 0;
     int k = 0;
 
-    for (int i = 1; i <= 16; i++){
+    for (int i = 1; i <= 16; i++) {
         k = input_bit(&read_bit, &bit_len, input_file, &garbage_bit);
         value = 2 * value + k;
     }
-    unsigned long long diff = high_v - low_v + 1;
-    for (;;) { // Read from input file
+    unsigned int diff = high_v - low_v + 1;
+    for (;;) {  // Read from input file
+        unsigned int freq = static_cast<unsigned int>(((
+            static_cast<unsigned int>(value) - low_v + 1)   * delitel - 1)
+                                    / (diff));
 
-        
-
-            
-        unsigned long long freq = (unsigned long long )((((unsigned long long )value - low_v + 1) * delitel - 1) / (diff));
-        
         int j;
-        for ( j = 1; table[j] <= freq; j++);
+        for ( j = 1; table[j] <= freq; j++) {}
         high_v = low_v +  table[j] * diff / delitel - 1;
         low_v = low_v + table[j - 1] * diff  / delitel;
 
-        for (;;){
-                 
-            if (high_v < half){
-                
-            }
-
-            else if (low_v >= half){
+        for (;;) {
+            if (high_v < half) {
+            } else if (low_v >= half) {
                 low_v -= half;
                 high_v -= half;
                 value -= half;
-            }
-
-            else if ((low_v >= first_qtr) && (high_v < third_qtr)){
+            } else if ((low_v >= first_qtr) && (high_v < third_qtr)) {
                 low_v -= first_qtr;
                 high_v -= first_qtr;
                 value -= first_qtr;
+            } else {
+                break;
             }
-            else break;
-            
+
             low_v += low_v;
             high_v += high_v + 1;
             k = 0;
-            k = input_bit(&read_bit, &bit_len, input_file, &garbage_bit);
+            k = input_bit(&read_bit, &bit_len,
+                            input_file, &garbage_bit);
             value += value + k;
-
-            
-
         }
-        
-        if (j==1){
+
+        if (j == 1) {
             break;
         }
 
         fputc(arr[j-2].first, output_file);
         diff = high_v - low_v + 1;
-        
-        
     }
+
     fclose(input_file);
     fclose(output_file);
 }
 
-
-unsigned int checker (const char* before_name="input.txt", const char* after_name="output.txt"){ // Checking for file matches 
+// Checking for file matches
+unsigned int checker(const char* before_name = "input.txt",
+                        const char* after_name = "output.txt") {
     unsigned int same = 0;
     FILE* before_file = fopen(before_name, "r");
     FILE* after_file = fopen(after_name, "r");
@@ -178,25 +156,23 @@ unsigned int checker (const char* before_name="input.txt", const char* after_nam
     while (!feof(after_file) && !feof(before_file)) {
         after_l = fgetc(after_file);
         before_l = fgetc(before_file);
-        if(!feof(after_file) && !feof(before_file)){
-            if(after_l != before_l){
+        if (!feof(after_file) && !feof(before_file)) {
+            if (after_l != before_l) {
                 same++;
             }
         }
     }
 
-    while (!feof(after_file))
-    {
+    while (!feof(after_file)) {
         after_l = fgetc(after_file);
-        if(!feof(after_file)){
+        if (!feof(after_file)) {
             same++;
         }
     }
 
-    while (!feof(before_file))
-    {
+    while (!feof(before_file)) {
         before_l = fgetc(before_file);
-        if(!feof(before_file)){
+        if (!feof(before_file)) {
             same++;
         }
     }
@@ -205,7 +181,7 @@ unsigned int checker (const char* before_name="input.txt", const char* after_nam
     return same;
 }
 
-int main (){
+int main() {
     decoder();
-    cout<<checker()<<endl;
+    std::cout << checker() << std::endl;
 }
